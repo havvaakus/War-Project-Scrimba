9def7f1fb180ace373bf7c3535210501144babb7
Havva Kuş <havvaakus@gmail.com>
2024-05-07 19:49:55 +0300

    .

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 document.getElementById("draw-cards").addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data) { 
+        .then(data => { 
+            console.log(data) 
             document.getElementById("cards").innerTML = `
                 <img src= ${data.cards[0].image} />
                 <img src= ${data.cards[1].image} />
